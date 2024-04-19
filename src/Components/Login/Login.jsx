4e9902f3e1a15c442c9e631c8f9ab8cad05772a7
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-04-20 02:02:18 +0530

    wrong password and wrong email is added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,19 +3,19 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import loginImg from "../../assets/Images/login1img.svg"
+import loginImg from "../../assets/Images/login1img.svg";
 
 import axios from "axios";
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login() {
   const [validated, setValidated] = useState(false);
-  const [userData ,setUserData]=useState({
-    email:'',
-    password:'',
-    role:'',
-  })
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
   const handleSubmit = async (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -26,51 +26,68 @@ function Login() {
     setValidated(true);
 
     event.preventDefault();
-    try{
-      const response= await axios.post('http://localhost:8000/user-login',userData);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/user-login",
+        userData
+      );
       console.log(response);
-      if(response.status===200){
-        toast.success('login successfully!!');
-        const token = localStorage.setItem("token",response.data.data.token)
-        setTimeout(()=>{
-          if (response.data.data.role === 'worker') {
+      if (response.status === 200) {
+        toast.success("login successfully!!");
+        const token = localStorage.setItem("token", response.data.data.token);
+        setTimeout(() => {
+          if (response.data.data.role === "worker") {
             window.location.href = `#/workerprofile/${response.data.data.user._id}`;
           }
-          if(response.data.data.role==='user'){
-            window.location.href = '#/user-details';
+          if (response.data.data.role === "user") {
+            window.location.href = "#/user-details";
           }
-        },2000)
+        }, 2000);
       }
-    }catch(error){
-        toast.error("login failed");
-        console.error("error in login:",error)
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        toast.error("Incorrect password. Please try again.");
+      } else if (
+        error.response &&
+        error.response.status === 404 &&
+        error.response.data === "Not Found"
+      ) {
+        toast.error("Email not registered. Please sign up.");
+      } else {
+        console.error("Error logging in:", error);
+        toast.error("Something went wrong. Please try again later.");
+      }
     }
-    
-
   };
 
-  const handleInputChange=(e)=>{
-    setUserData({...userData,[e.target.name]:e.target.value})
+  const handleInputChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
     console.log(userData);
-  }
+  };
   return (
     <>
       <div className="container">
         <div className="row justify-content-center align-items-center">
           <div className="col-md-5 col-11">
-              <img src={loginImg} alt="" />
+            <img src={loginImg} alt="" />
           </div>
           <div className="col-md-5 col-11">
             <div className="card p-10 m-auto">
               <h4 className="font-bold mb-4 text-xl">Login</h4>
-              <Form noValidate validated={validated} onSubmit={handleSubmit} method="post" action='/login'>
+              <Form
+                noValidate
+                validated={validated}
+                onSubmit={handleSubmit}
+                method="post"
+                action="/login"
+              >
                 <Form.Group className="mb-3" controlId="validationCustom01">
                   <Form.Label>Username or email address</Form.Label>
                   <Form.Control
                     required
                     type="test"
                     placeholder="Username or email address"
-                    name='email'
+                    name="email"
                     onChange={handleInputChange}
                     value={userData.email}
                   />
@@ -81,7 +98,7 @@ function Login() {
                     type="password"
                     placeholder="Password"
                     required
-                    name='password'
+                    name="password"
                     onChange={handleInputChange}
                     value={userData.password}
                   />
